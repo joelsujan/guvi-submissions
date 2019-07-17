8302945bf768c4ab6fc100bb9ea16c2ca439dbcf
joelsujan <52925895+joelsujan@users.noreply.github.com>
2019-07-17 18:51:23 +0530

    vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,27 @@
+#include<isotream>
+#include<cstring>
+int main()
+{
+char as;
+cin>>as;
+switch(as)
+{ case 'a':
+cout<<"vowel";
+break;
+ case 'e':
+cout<<"vowel";
+break;
+ case 'i':
+cout<<"vowel";
+break;
+ case 'o':
+cout<<"vowel";
+break;
+ case 'u':
+cout<<"vowel";
+break;
+ default: 
+ cout<<"consanat"
+ }
+ return 0;
+ }
