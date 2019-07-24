31b4b5cb0173a0a464b6cf6abede3c48fc9ad37d
joelsujan <52925895+joelsujan@users.noreply.github.com>
2019-07-24 14:22:32 +0530

    Create alpha.cpp

---
+++ alpha.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char a;
+cin>>a;
+if ((a >= 65 && ar <= 90) 
+        || (a >= 97 && a <= 122)) 
+        cout << " Alphabet "; 
+  return 0;
+  }
